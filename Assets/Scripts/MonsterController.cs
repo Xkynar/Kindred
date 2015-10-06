74ef53658342ff874956690f0b4ac4c0519b1c97
Xkynar <xkynar@gmail.com>
2015-10-06 14:45:16 +0100

    FIrst Commit

---
+++ Assets/Scripts/MonsterController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MonsterController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start ()
+    {
+
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+
+    }
+
+
+    void OnMouseDown()
+    {
+        Debug.Log("Poke");
+        transform.Translate(1, 0, 0);
+    }
+}
