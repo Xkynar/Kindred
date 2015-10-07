9841cecccdf06d20b6b234a6ce9b23ddc0aeed34
Vasco Gomes <ovascogomes@gmail.com>
2015-10-07 15:12:42 +0100

    Added PlayerNetworkManager

---
+++ Assets/Scripts/PlayerNetworkManager.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+using System.Collections;
+
+public class PlayerNetworkManager : NetworkBehaviour
+{
+    [SyncVar] public string squadList;
+
+    void Start()
+    {
+        if (isLocalPlayer)
+        {
+            CmdSyncSquadList(Random.Range(1, 100).ToString());
+        }
+
+        Debug.Log("Player " + netId + ": " + squadList);
+    }
+
+    [Command]
+    void CmdSyncSquadList(string squadList)
+    {
+        this.squadList = squadList;
+    }
+
+    void Update()
+    {
+        if (isLocalPlayer && Input.GetMouseButtonDown(0))
+        {
+            RpcDoSomething();
+        }
+    }
+
+    [ClientRpc]
+    void RpcDoSomething()
+    {
+        Debug.Log("Player " + netId + " clicked a button.");
+    }
+}
