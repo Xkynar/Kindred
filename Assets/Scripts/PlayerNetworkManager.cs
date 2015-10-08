cb67334b638cee7a14189a22db68902188987c07
Vasco Gomes <ovascogomes@gmail.com>
2015-10-08 15:36:09 +0100

    Added spawn squad method

--- Assets/Scripts/PlayerNetworkManager.cs
+++ Assets/Scripts/PlayerNetworkManager.cs
@@ -5,12 +5,17 @@ using System.Collections;
 public class PlayerNetworkManager : NetworkBehaviour
 {
     [SyncVar] public string squadList;
+    [SyncVar] public string side;
+
+    public GameObject monsterExample;
 
     void Start()
     {
+        PlayerPrefs.SetString("side", "LEFT");
+
         if (isLocalPlayer)
         {
-            CmdSyncSquadList(Random.Range(1, 100).ToString());
+            CmdSyncSetup(Random.Range(1, 100).ToString(), PlayerPrefs.GetString("side"));
         }
 
         StartCoroutine("Setup");
@@ -23,13 +28,38 @@ public class PlayerNetworkManager : NetworkBehaviour
             yield return null;
         }
 
-        Debug.Log("Player " + netId + " Squad: " + squadList);
+        SpawnSquad();
+    }
+
+    void SpawnSquad()
+    {
+        GameObject squadParent = null;
+        int squadCount = 5;
+        float squadSpacing = 0.2f;
+        float initialPos = -squadSpacing * (squadCount - 1) / 2;
+
+        if (side == "LEFT")
+        {
+            squadParent = GameObject.FindGameObjectWithTag("LeftSquad");
+        }
+        else if (side == "RIGHT")
+        {
+            squadParent = GameObject.FindGameObjectWithTag("RightSquad");
+        }
+
+        for (int i = 0; i < squadCount; i++)
+        {
+            GameObject monster = Instantiate(monsterExample, Vector3.zero, Quaternion.identity) as GameObject;
+            monster.transform.parent = squadParent.transform;
+            monster.transform.localPosition = new Vector3(initialPos + squadSpacing * i, 0f, 0f);
+        }
     }
 
     [Command]
-    void CmdSyncSquadList(string squadList)
+    void CmdSyncSetup(string squadList, string side)
     {
         this.squadList = squadList;
+        this.side = side;
     }
 
     [Command]
@@ -44,6 +74,7 @@ public class PlayerNetworkManager : NetworkBehaviour
         Debug.Log("Player " + netId + " clicked a button.");
     }
 
+    [ClientCallback]
     void Update()
     {
         if (isLocalPlayer && Input.GetMouseButtonDown(0))
