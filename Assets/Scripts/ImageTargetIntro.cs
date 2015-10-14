f7e7533d870998c087e31532847df5a820637215
Xkynar <xkynar@gmail.com>
2015-10-14 17:17:38 +0100

    Adapted project to new requirements
    New database, targets, utility scripts.

---
+++ Assets/Scripts/ImageTargetIntro.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using Vuforia;
+
+public class ImageTargetIntro : MonoBehaviour, ITrackableEventHandler
+{
+    private TrackableBehaviour mTrackableBehaviour;
+
+    void Start()
+    {
+        mTrackableBehaviour = GetComponent<TrackableBehaviour>();
+
+        if (mTrackableBehaviour)
+        {
+            mTrackableBehaviour.RegisterTrackableEventHandler(this);
+        }
+    }
+
+    public void OnTrackableStateChanged(TrackableBehaviour.Status previousStatus, TrackableBehaviour.Status newStatus)
+    {
+        if (newStatus == TrackableBehaviour.Status.DETECTED ||
+            newStatus == TrackableBehaviour.Status.TRACKED ||
+            newStatus == TrackableBehaviour.Status.EXTENDED_TRACKED)
+        {
+            Debug.Log("RAAAAAAAAAAAAAAAAAAAAAAAAAAWR");
+        }
+        else
+        {
+            // target is lost
+        }
+    }
+}
---
+++ Assets/Scripts/TrackableList.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using Vuforia;
+
+public class TrackableList : MonoBehaviour
+{
+    void Start()
+    {
+        InvokeRepeating("CheckTargets", 10f, 10f);
+    }
+
+    void CheckTargets()
+    {
+        // Get the StateManager
+        StateManager sm = TrackerManager.Instance.GetStateManager();
+
+        // Query the StateManager to retrieve the list of
+        // currently 'active' trackables 
+        //(i.e. the ones currently being tracked by Vuforia)
+        IEnumerable<TrackableBehaviour> activeTrackables = sm.GetActiveTrackableBehaviours();
+
+        // Iterate through the list of active trackables
+        Debug.Log("List of trackables currently active (tracked): ");
+        int numFrameMarkers = 0;
+        int numImageTargets = 0;
+        int numMultiTargets = 0;
+        int numObjectTargets = 0;
+        foreach (TrackableBehaviour tb in activeTrackables)
+        {
+            Debug.Log("Trackable: " + tb.TrackableName);
+
+            if (tb is MarkerBehaviour)
+                numFrameMarkers++;
+            else if (tb is ImageTargetBehaviour)
+                numImageTargets++;
+            else if (tb is MultiTargetBehaviour)
+                numMultiTargets++;
+            else if (tb is ObjectTargetBehaviour)
+                numObjectTargets++;
+        }
+
+        // Debug.Log("Found " + numFrameMarkers + " frame markers in curent frame");
+        Debug.Log("Found " + numImageTargets + " image targets in curent frame");
+        // Debug.Log("Found " + numMultiTargets + " multi-targets in curent frame");
+        // Debug.Log("Found " + numObjectTargets + " object-targets in current frame");
+    }
+}
