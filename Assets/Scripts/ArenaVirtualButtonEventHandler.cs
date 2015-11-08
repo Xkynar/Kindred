3306c616059803370c28b0c5f5fa122e61012787
Xkynar <xkynar@gmail.com>
2015-11-08 19:29:07 +0000

    Added Virtual Button (center of arena)

---
+++ Assets/Scripts/ArenaVirtualButtonEventHandler.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using Vuforia;
+
+public class ArenaVirtualButtonEventHandler : MonoBehaviour, IVirtualButtonEventHandler {
+
+	// Use this for initialization
+	void Start ()
+    {
+        // Search for all Children from this ImageTarget with type VirtualButtonBehaviour
+        VirtualButtonBehaviour[] vbs = GetComponentsInChildren<VirtualButtonBehaviour>();
+        for (int i = 0; i < vbs.Length; ++i)
+        {
+            // Register with the virtual buttons TrackableBehaviour
+            vbs[i].RegisterEventHandler(this);
+        }
+	}
+
+    void IVirtualButtonEventHandler.OnButtonPressed(VirtualButtonAbstractBehaviour vb)
+    {
+        Debug.Log("pressed");
+    }
+
+    void IVirtualButtonEventHandler.OnButtonReleased(VirtualButtonAbstractBehaviour vb)
+    {
+        Debug.Log("released");
+    }
+}
